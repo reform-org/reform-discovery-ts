304f9e3e8be35f939d0320a6e82335f5c2a6356f
Lukas Schreiber <info@lukasschreiber.com>
2023-03-10 16:02:18 +0100

    deployment

--- index.ts
+++ index.ts
@@ -4,10 +4,10 @@ import webSocketServer from './wss/socket';
 
 dotenv.config();
 
-app.listen(process.env.API_PORT || 3000, () => {
+app.listen(process.env.VITE_DISCOVERY_SERVER_LISTEN_PORT || 3000, () => {
   console.log(`REST server listening on port ${process.env.VITE_DISCOVERY_SERVER_LISTEN_PORT || 3000}`);
 });
 
-webSocketServer.listen(process.env.WSS_PORT || 7071, () => {
+webSocketServer.listen(process.env.VITE_DISCOVERY_SERVER_WEBSOCKET_LISTEN_PORT || 7071, () => {
   console.log(`WSS server listening on port ${process.env.VITE_DISCOVERY_SERVER_WEBSOCKET_LISTEN_PORT || 7071}`);
 });
